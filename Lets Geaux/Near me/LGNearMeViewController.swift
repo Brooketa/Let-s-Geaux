35427617ce5d43dd5020839c700321a949dc3547
Bruno Krolo <bruno.krolo@gmail.com>
2021-04-15 18:04:58 +0200

    - Add classes for Near me and Prize ViewControllers

---
+++ Lets Geaux/Near me/LGNearMeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LGNearMeViewController.swift
+//  Lets Geaux
+//
+//  Created by Brooketa on 15.04.2021..
+//
+
+import UIKit
+
+class LGNearMeViewController: LGViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+    }
+}
---
+++ Lets Geaux/Prizes/LGPrizesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LGPrizesViewController.swift
+//  Lets Geaux
+//
+//  Created by Brooketa on 15.04.2021..
+//
+
+import UIKit
+
+class LGPrizesViewController: LGViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
