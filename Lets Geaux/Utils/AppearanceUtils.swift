cdcd0291019aae4e77549d12bb3fb50256a665a9
Bruno Krolo <bruno.krolo@gmail.com>
2022-07-07 00:49:07 +0200

    - Removed unnecessary colors from AppereanceUtils - Codestyle update for APIConstants struct

--- Lets Geaux/Utils/AppearanceUtils.swift
+++ Lets Geaux/Utils/AppearanceUtils.swift
@@ -2,9 +2,6 @@ import Foundation
 import UIKit
 
 struct AppearanceUtils {
-    static let turquoise = UIColor(red: 69/255, green: 163/255, blue: 177/255, alpha: 1.0)
-    static let lightGrey = UIColor(red: 238/255, green: 239/255, blue: 243/255, alpha: 1.0)
-    static let darkGrey = UIColor(red: 74/255, green: 74/255, blue: 74/255, alpha: 1.0)
 
     static let eventTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.accentColor, NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 13)]
 
