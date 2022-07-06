d498691b672d8498af38d74ecfe156ad2d57caaa
Bruno Krolo <72989070+Brooketa@users.noreply.github.com>
2022-07-07 00:49:39 +0200

    - Codestyle fixes (#14)
    * - Codestyle fixes
    - Removed unused struct DiscoverCollectionViewConstants
    
    * - Removed unnecessary colors from AppereanceUtils
    - Codestyle update for APIConstants struct

--- Lets Geaux/Utils/APIConstants.swift
+++ Lets Geaux/Utils/APIConstants.swift
@@ -1,6 +1,8 @@
 struct APIConstants {
+
     static let venueRequestURL = "https://api.ajmo.hr/v3/venue/dashboard"
     static let eventRequestURL = "https://api.ajmo.hr/v3/event/dashboard"
     static let promotedNewsRequestURL = "https://api.ajmo.hr/v3/news/index?isPromoted=1"
     static let newsRequestURL = "https://api.ajmo.hr/v3/news/index?isPromoted=0&amp;page=1&amp;perPage=10"
+
 }
